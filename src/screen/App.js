aed1e5b94ab5d91e5cf494e09d4fb329d06bdc0b
Aman <amanchouhan2801@gmail.com>
2024-09-20 16:09:33 +0530

    First

--- src/screen/App.js
+++ src/screen/App.js
@@ -31,7 +31,7 @@ function App() {
         <svg height="60" width="320" xmlns="http://www.w3.org/2000/svg">
           <rect className="shape" height="60" width="320" />
         </svg>        
-        <motion.p variants={loadText} initial='hidden' animate='visible' className="text"> **Aman Chouhan** </motion.p>
+        <motion.p variants={loadText} initial='hidden' animate='visible' className="text"> **Aman Chuhan** </motion.p>
       </div>
     </div>
     :
